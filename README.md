41f4e7b8a6a9c42060274c04964e6f74ab99ea39
WeRockStar <kotchaphan.m@gmail.com>
2024-09-15 16:22:06 +0700

    update to generate content

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # LangChainGo
 
-Learn How to build LLMs apps with Go and [LangChainGo](https://github.com/tmc/langchaingo)
+Learn How to build LLMs apps with Go and [LangChainGo](https://github.com/tmc/langchaingo) - In Progress
 
 ## Prequsites
 
--- main.go
+++ main.go
@@ -15,16 +15,14 @@ func main() {
 		log.Fatal(err)
 	}
 	ctx := context.Background()
-	completion, err := llm.Call(ctx, "Human: Who is win Ballon d'Or 2021??\nAssistant:",
-		llms.WithTemperature(0.2),
-		llms.WithStreamingFunc(func(ctx context.Context, chunk []byte) error {
-			fmt.Print(string(chunk))
-			return nil
-		}),
-	)
+	output, err := llm.GenerateContent(ctx, []llms.MessageContent{
+		llms.TextParts(llms.ChatMessageTypeSystem, "Ballon d'Or is an annual football award presented by France Football. It has been awarded since 1956, although between 2010 and 2015, an agreement was made with FIFA, and the award was temporarily merged with the FIFA World Player of the Year award and known as the FIFA Ballon d'Or. However, the partnership ended in 2016, and the award was reverted to the Ballon d'Or, while FIFA also reverted to its own separate annual award. The award is presented to the best"),
+		llms.TextParts(llms.ChatMessageTypeHuman, "Who is win Ballon d'Or 2021?"),
+	})
 	if err != nil {
 		log.Fatal(err)
 	}
-
-	_ = completion
+	for i := 0; i < len(output.Choices); i++ {
+		fmt.Println(output.Choices[i].Content)
+	}
 }
